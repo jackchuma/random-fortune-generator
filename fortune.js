18876c2f9ae82918d5108e0219c6a4812dbff8e0
John Chuma <johnchuma@Johns-Air.westell.com>
2021-02-08 16:18:08 -0500

    created messageComponents object with 3 entries each in occupations, city, and residence

---
+++ fortune.js
@@ -0,0 +1,7 @@
+//Create an object to store possible outputs for each category of the fortune
+const messageComponents = {
+    occupations: ['nurse', 'engineer', 'teacher'],
+    city: ['Boston, MA', 'New York, NY', 'Los Angeles, CA'],
+    residence: ['apartment', 'condo', 'house']
+}
+
